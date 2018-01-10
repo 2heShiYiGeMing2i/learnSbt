9fa597a0446b93195900cf95ce68cbea648ca3c8
赵磊 <zhaolei@begowin.com>
2018-01-10 15:54:14 +0800

    list

--- src/test/scala/partthree/TryCatchTest.scala
+++ src/test/scala/partthree/TryCatchTest.scala
@@ -6,50 +6,103 @@ import org.scalatest.{FlatSpec, Matchers}
   * Created by zhaolei on 2018/1/9
   */
 class TryCatchTest extends FlatSpec with Matchers {
-
-  //  object Greeting {
-  //    def english = "Hi"
+  //  val aa: List[String] = Nil
+  //  val b: List[Int] = Nil
   //
-  //    def espanol = "Hola"
+  //  (aa == Nil) should be(
+  //    true
+  //  )
+  //  (aa equals Nil) should be(
+  //    true
+  //  )
+  //
+  //  (b == Nil) should be(
+  //    true
+  //  )
+  //  (b eq Nil) should be(
+  //    true
+  //  )
+  //
+  //  (aa == b) should be(
+  //    true
+  //  )
+  //  (aa eq b) should be(
+  //    true
+  //  )
+
+  //  val aa = List(1, 2, 3)
+  //  aa.headOption should equal(Some(1))
+  //  aa.tail should equal(List(2, 3))
+  //  System.err.println(aa.headOption)
+  //  private val list = List(1)
+  //  println(list.tail)
+  //  System.err.println(aa.tail.tail.tail.tail)
+  //
+  //  val aa = List(1, 3, 5, 7, 9)
+  //  aa(0) should equal(
+  //    1
+  //  )
+  //  aa(2) should equal(
+  //    5
+  //  )
+  //  aa(4) should equal(
+  //    9
+  //  )
+  //
+  //  intercept[IndexOutOfBoundsException] {
+  //    println(aa(5))
   //  }
+
+
+  //  val aa = List(1, 3, 5, 7, 9)
+  //  val b = aa.filterNot(v ⇒ v == 5) // remove where value is 5
   //
-  //  val x = Greeting
-  //  val y = x
-  //  System.err.println(x eq y)
+  //  aa should equal(List(1, 3, 5, 7, 9))
+  //  b should equal(List(1, 3, 7, 9))
+
+  //  var aa = List(1, 3, 5, 7, 9)
   //
-  //  val z = Greeting
+  //  // get the length of the list
+  //  aa.length should equal(5)
   //
-  //  System.err.println(x eq z)
-  def addWithoutSyntaxSugar(x: Int) = (x: Int, y: Int) => x + y
+  //  // reverse the list
+  //  aa = aa.reverse
+  //  aa should equal(
+  //    List(9, 7, 5, 3, 1)
+  //  )
+  //
+  //  // map a function to double the numbers over the list
+  //  aa = aa.map { v ⇒
+  //    v * 2
+  //  }
+  //  aa should equal(
+  //    List(18, 14, 10, 6, 2)
+  //  )
+  //
+  //  // filter any values divisible by 3 in the list
+  //  aa.filter { v ⇒
+  //    v % 3 == 0
+  //  } should equal(
+  //    List(18, 6)
+  //  )
 
-  //  System.err.println(addWithoutSyntaxSugar(1).isInstanceOf[(_, _) => _])
+  //  val aa = List(1, 3, 5, 7)
+  //  // NOTE: foldLeft uses a form called currying that we will explore later
+  //  aa.foldLeft(0)(_ + _) should equal(16)
+  //  aa.sum should equal(16)
+  //  aa.foldLeft(10)(_ + _) should equal(26)
+  //  aa.foldLeft(1)(_ * _) should equal(105)
+  //  aa.product should equal(105)
+  //  aa.foldLeft(0)(_ * _) should equal(0)
 
-  def makeUpper(xs: List[String]) = xs map {
-    _.toUpperCase
-  }
+  val d = Nil
+  val c = 3 :: d
+  val b = 2 :: c
+  val aa = 1 :: b
 
-  def makeWhatEverYouLike(xs: List[String], sideEffect: String ⇒ String) =
-    xs map sideEffect
-
-
-  makeUpper(List("abc", "xyz", "123")) should be(
-    List("ABC", "XYZ", "123")
-  )
-
-  //using it inline
-  val myName = (name: String) => s"My name is $name"
-  makeWhatEverYouLike(List("John", "Mark"), myName) should be(List("My name is John", "My name is Mark"))
-
-
-  List("Scala", "Erlang", "Clojure") map (_.length) should be(
-    List(5,6,7)
-  )
-  makeWhatEverYouLike(List("ABC", "XYZ", "123"), { x ⇒
-    x.toLowerCase
-  }) should be(
-    List("abc", "xyz", "123")
-  )
-
-
+  aa should be(List(1, 2, 3))
+  aa.tail should be(b)
+  b.tail should be(c)
+  c.tail should be(d)
 
 }

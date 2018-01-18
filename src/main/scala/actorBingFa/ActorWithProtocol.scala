675257ff9a8bb1004d97d23d85a5db09aceddf69
赵磊 <zhaolei@begowin.com>
2018-01-18 10:37:00 +0800

    actor stash

---
+++ src/main/scala/actorBingFa/ActorWithProtocol.scala
@@ -0,0 +1,37 @@
+package actorBingFa
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Stash}
+
+/**
+  * Created by zhaolei on 2018/1/18
+  */
+class ActorWithProtocol extends Actor with Stash {
+  def receive = {
+    case "open" ⇒
+      System.err.println("1 open")
+      unstashAll()
+      context.become({
+        case "write" ⇒
+          System.err.println("2 write")
+        case "close" ⇒
+          System.err.println("3 close")
+          unstashAll()
+          context.unbecome()
+        case msg     ⇒ System.err.println("4 other"); stash()
+      }, discardOld = false)
+    case "done" ⇒ System.err.println("5 done")
+    case msg    ⇒ System.err.println("6"); stash()
+  }
+}
+
+object ActorWithProtocol extends App {
+  private val system = ActorSystem("system")
+  private val actor1: ActorRef = system.actorOf(Props(new ActorWithProtocol), "actor1")
+  actor1.tell("open", actor1)
+  actor1 ! "write"
+  actor1 ! "close"
+  //  actor1 ! "open"
+  actor1 ! "done"
+  actor1 ! "done"
+  actor1 ! "done"
+}
---
+++ src/main/scala/actorBingFa/Example_15.scala
@@ -0,0 +1,44 @@
+package actorBingFa
+
+import akka.actor.TypedActor
+import akka.actor.TypedActor.{PostStop, PreStart}
+import akka.event.Logging
+
+import scala.concurrent.{Future, Promise}
+
+/**
+  * Created by zhaolei on 2018/1/17
+  */
+object Example_15 extends App {
+
+  trait MyType{
+    //fire-and-forget消息
+    def squareDontCare(i: Int): Unit
+    //非阻塞send-request-reply消息
+    def square(i: Int): Future[Int]
+    //阻塞式的send-request-reply消息
+    def squareNowPlease(i: Int): Option[Int]
+    //阻塞式的send-request-reply消息
+    def squareNow(i: Int): Int
+  }
+
+  //混入PostStop和PreStart
+  class SquarerImpl(val name: String) extends  MyType with PostStop with PreStart {
+    import akka.actor.TypedActor.context
+    val log = Logging(context.system,TypedActor.self.getClass)
+    def this() = this("SquarerImpl")
+
+    def squareDontCare(i: Int): Unit = i * i
+    def square(i: Int): Future[Int] = Promise.successful(i * i).future
+    def squareNowPlease(i: Int): Option[Int] = Some(i * i)
+    def squareNow(i: Int): Int = i * i
+
+    def postStop(): Unit={
+      log.info ("TypedActor Stopped")
+    }
+    def preStart(): Unit={
+      log.info ("TypedActor  Started")
+    }
+  }
+
+}

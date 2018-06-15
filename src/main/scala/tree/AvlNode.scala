f21381eaa591ef02961b52febdb0265d71b462c5
zhaolei <zhaolei@begowin.com>
2018-06-15 15:28:36 +0800

    平衡节点

--- src/main/scala/tree/AvlNode.scala
+++ src/main/scala/tree/AvlNode.scala
@@ -1,7 +1,6 @@
 package tree
 
 import java.util
-import java.util.Collections
 
 /**
   * Created by zhaolei on 2018/6/13
@@ -62,14 +61,92 @@ object RotateAvlNode {
   }
 
   /**
+    * 添加
+    */
+
+  def add[K <: Comparable[K], V](key: K, value: V, tree: AvlNode[K, V]): AvlNode[K, V] = {
+    if (tree == null) return new AvlNode(key, value, null, null)
+    // 左子树
+    var retTree: AvlNode[K, V] = tree
+    if (key.compareTo(retTree.key) < 0) {
+      retTree.left = add(key, value, retTree.left)
+      if (height(retTree.left) - height(retTree.right) == 2) {
+        if (key.compareTo(retTree.left.key) < 0) retTree = rotateLL(retTree)
+        else retTree                                     = rotateLR(retTree)
+      }
+    }
+    // 右子树
+    if (key.compareTo(retTree.key) > 0) {
+      retTree.right = add(key, value, retTree.right)
+      if (height(retTree.right) - height(retTree.left) == 2) {
+        if (key.compareTo(retTree.right.key) > 0) retTree = rotateRR(retTree)
+        else retTree                                      = rotateRL(retTree)
+      }
+    }
+
+    // 相等
+    if (key.compareTo(retTree.key) == 0) {
+      retTree.attach += value
+    }
+    // 计算高度
+    retTree.height = height(retTree.left).max(height(retTree.right)) + 1
+    retTree
+  }
+
+  def delete[K <: Comparable[K], V](key: K, value: V, tree: AvlNode[K, V]): AvlNode[K, V] = {
+    if (tree == null) return null
+    // 点在左子树
+    var retTree: AvlNode[K, V] = tree
+    if (key.compareTo(retTree.key) < 0) {
+      retTree.left = delete(key, value, retTree.left)
+      if (height(retTree.right) - height(retTree.left) == 2) { // 右子树比左子树高
+        if (height(retTree.right.left) - height(retTree.right.right) > 0) retTree = rotateRL(retTree) // 右子树的左子树高与右子树
+        else retTree                                                              = rotateRR(retTree)
+      }
+    }
+    // 点在右子树
+    if (key.compareTo(retTree.key) > 0) {
+      retTree.right = delete(key, value, retTree.right)
+      if (height(retTree.left) - height(retTree.right) == 2) { // 左树高于右树
+        if (height(retTree.left.left) - height(retTree.left.right) > 0) retTree = rotateLL(retTree) // 左树的左树 高于左树的右树
+        else retTree                                                            = rotateLR(retTree)
+      }
+
+    }
+    // 当前节点
+    if (key.compareTo(retTree.key) == 0) {
+      if (retTree.right != null) { // 右子树不为空
+        var tmpRight = retTree.right
+        while (tmpRight.left != null) tmpRight = tmpRight.left
+        retTree.value  = tmpRight.value
+        retTree.height = tmpRight.height
+        retTree.right  = delete(retTree.key, tmpRight.value, retTree)
+      } else {}
+    }
+    retTree.height = height(retTree.left).max(height(retTree.right)) + 1
+    retTree
+
+  }
+
+  /**
     * 当前节点的高度
     */
-  def height[K, V](node: AvlNode[K, V]): Int = ???
+  def height[K, V](node: AvlNode[K, V]): Int = if (node == null) -1 else node.height
 
 }
 
 object AvlNode extends App {
-  private val list: util.List[Int] = Collections.emptyList()
-  list.add(100)
-  System.err.println(list)
+  private val ints = new util.ArrayList[String](10)
+  ints.add(0, 0.toString)
+  ints.add(1, 1.toString)
+  ints.add(2, 2.toString)
+  ints.add(3, 3.toString)
+  ints.add(4, 4.toString)
+  ints.add(5, 5.toString)
+  ints.add(6, 6.toString)
+  ints.add(7, 7.toString)
+  ints.add(8, null)
+  System.err.println(ints)
+  System.err.println(ints.indexOf(null))
+
 }

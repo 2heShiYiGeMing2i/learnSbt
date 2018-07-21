5703ae4276ef130c0198d4cd80e0c21f4010cc87
zhaolei <zhaolei@begowin.com>
2018-07-21 13:44:00 +0800

    quartz2 simple trigger

--- src/main/scala/jAvA/HellowQuartz/HelloScheduler.java
+++ src/main/scala/jAvA/HellowQuartz/HelloScheduler.java
@@ -5,6 +5,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by zhaolei on 2018/7/21
@@ -15,23 +16,24 @@ public class HelloScheduler {
         JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
                 .withIdentity("myjob", "group1")
                 .build();
-        Calendar startCalendar = Calendar.getInstance();
-        startCalendar.add(Calendar.SECOND, 3);
-        Calendar endCalendar = Calendar.getInstance();
-        endCalendar.add(Calendar.SECOND, 6);
+        Date startDate = new Date();
+        Date endDate = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        System.err.println("current time is" + sf.format(startDate.getTime()));
+        startDate.setTime(startDate.getTime() + 4000L);
+        endDate.setTime(endDate.getTime() + 6000L);
 
         // 执行 规则
-        Trigger trigger = TriggerBuilder
+        SimpleTrigger trigger = (SimpleTrigger) TriggerBuilder
                 .newTrigger()
                 .withIdentity("mytrigger", "triggerGroup")
-                .startAt(startCalendar.getTime())
-                .endAt(endCalendar.getTime())
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever())
+                .startAt(startDate)
+                .endAt(endDate)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).withRepeatCount(3))
                 .build();
 
 
         java.util.Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
         System.err.println("mian current time is" + sf.format(calendar.getTime()));
 
         SchedulerFactory sfact = new StdSchedulerFactory();
